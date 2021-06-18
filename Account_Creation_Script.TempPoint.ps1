567cf8c59e30a80bfe46a37d04aa2abf756c69b9
ben@WHARTON-BEN.LOCAL <ben@WHARTON-BEN.LOCAL>
2021-06-18 17:21:46 -0500

    Modified the Set-DSPassword Function

--- Account_Creation_Script.TempPoint.ps1
+++ Account_Creation_Script.TempPoint.ps1
@@ -1,45 +1,47 @@
-﻿Function Set-DSPassword
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2021 v5.8.187
+	 Created on:   	6/18/2021 3:42 PM
+	 Created by:   	Ben
+	 Organization: 	
+	 Filename:     	
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>
+
+#region - Set-Password Function
+Function Set-Password
 {
-	[CmdletBinding()]
-	param (
-		[parameter(Mandatory = $true,
-				   ValueFromPipeline = $true)]
-		[string]$Path,
-		[parameter(Mandatory = $true)]
-		[int]$minlength
-	)
-	
 	<#The $Dictionary and $Dictionary2 variables will contain a random word from a list that is stored in a .txt file
-		*The $Special variable will contain a special character from a list
-		*The $Number variable will contain a random number from 0 - 9
-		*If the password is too short, a number will be appended to the password until the password meets the requirements from the domain
-		*Once the password is the correct length, a special character will be added to the end of the password.
+	*The $Special variable will contain a special character from a list
+	*The $Number variable will contain a random number from 0 - 9
+	*If the password is too short, a number will be appended to the password until the password meets the requirements from the domain
+	*Once the password is the correct length, a special character will be added to the end of the password.
 	#>
-	begin
+	$pwlength = $config.'pwlength'
+	$Password = $null
+	
+	#Grab the words from the dictionary file and create a password
+	$Dictionary = Get-Content -Path $config.'dictionarypath' | Get-Random -Count 1
+	$Dictionary2 = Get-Content -Path $config.'dictionarypath' | Get-Random -Count 1
+	$Special = Get-Random ('!', '?')
+	$Number = Get-Random -Minimum 0 -Maximum 10
+	$Password = $Dictionary + $Dictionary2 + $Number
+	if ($Password.length -le ($pwlength - 1))
 	{
-		$Dictionary = Get-Content -Path $Path | Get-Random -Count 1
-		$Dictionary2 = Get-Content -Path $Path | Get-Random -Count 1
-		$Special = Get-Random ('!', '?')
-		$Number = Get-Random -Minimum 0 -Maximum 10
-		$Password = $Dictionary + $Dictionary2 + $Number
-	}
-	process
-	{
-		if ($Password.length -lt ($minlength - 1))
+		do
 		{
-			do
-			{
-				$Number = Get-Random -Minimum 0 -Maximum 10
-				$Password = $Password + $Number
-			}
-			while ($Password.length -lt ($minlength - 1))
-		}$Password = $Password + $Special
-	}
-	end
-	{
-		return $Password
-	}
+			$Number = Get-Random -Minimum 0 -Maximum 10
+			$Password = $Password + $Number
+		}
+		while ($Password.length -lt ($pwlength - 1))
+	}$Password = $Password + $Special
+	return $Password
 }
+#endregion
+
 #region - Add-VAAccount
 function Add-VAAccount
 {
